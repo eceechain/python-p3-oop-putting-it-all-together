3d958679918169b297e24702382af54055dd0660
ecee <you@example.com>
2023-11-29 14:16:07 +0300

    Done with assignment

--- lib/book.py
+++ lib/book.py
@@ -6,21 +6,23 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self._page_count = page_count
-        self._page_count = 0
     
+    @property
     def get_page_count(self):
-        print("Getting page count")
         return self._page_count
-
-    def set_page_count(self, page_count):
+    
+    @get_page_count.setter
+    def page_count(self, page_count):
         if type(page_count) == int:
-            print(f"Setting page count to {page_count}")
             self._page_count = page_count
         else:
             print("page_count must be an integer")
 
     def turn_page(self):
-        print("Flipping the page... wow, you read fast!")
+        print("Flipping the page...wow, you read fast!")
+
+
+    # turn_page(7)
     
-    _page_count = property(get_page_count, set_page_count)
+    # _page_count = property(get_page_count, set_page_count)
 
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,41 +1,19 @@
 class Shoe:
     def __init__(self, brand, size):
-        self._brand = None
-        self._size = None
-        self._condition = 'New'
-        self.set_brand(brand)
-        self.set_size(size)
+        self.brand = brand
+        self.size = size
 
-    def set_brand(self, value):
-        if type(value) == str:
-            self._brand = value
-        else:
-            print("Brand must be a string")
-
-    def get_brand(self):
-        return self._brand
-
-    def set_size(self, value):
-        if type(value) == int:
-            self._size = value
-        else:
-            print("Size must be an integer")
-
+    @property
     def get_size(self):
         return self._size
-
-    def repair_shoe(self):
-        print("The shoe has been repaired.")
-        self._condition = 'New'
-
-    def get_condition(self):
-        return self._condition
-
-# Example usage:
-shoe_instance = Shoe("Adidas", 9)
-print(shoe_instance.get_brand())          # Output: Adidas
-print(shoe_instance.get_size())           # Output: 9
-print(shoe_instance.get_condition())      # Output: New
-
-shoe_instance.repair_shoe()
-print(shoe_instance.get_condition())      # Output: New
+    
+    @get_size.setter
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+     
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
